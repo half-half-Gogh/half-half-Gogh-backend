3834514ad2e0701b7192fbb8b333cdf8f3b9948c
alexjun12 <alexjun12@naver.com>
2022-08-23 21:46:47 +0900

    asd

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require("../../routes/user/userController/serviceSignin");
-const userController2 = require("../../routes/user/userController/serviceLogin");
+const userController = require("./userController/serviceSignin");
+const userController2 = require("./userController/serviceLogin");
 
 router.post("/signin", userController.signIn);
 router.post("/login", userController2.logIn);
