dafe177aeb71b6e4cdc04fa12f7f29e0fa01bc07
alexjun12 <alexjun12@naver.com>
2022-09-11 22:32:12 +0900

    like service complete

--- routes/image/imgController/pressLike.js
+++ routes/image/imgController/pressLike.js
@@ -1,21 +1,16 @@
 const admin = require("../../../Configuration/firebaseAuthConfig");
 const db = admin.firestore();
 
-exports.pressLike = (req, res) => {
+exports.pressLike = async (req, res) => {
   const galleryName = req.body.galleryName;
   const imgId = req.body.imgId;
   const liker = req.body.liker;
-  console.log("press On");
-  db.collection(galleryName)
-    .doc(imgId)
-    .add({
-      // error!!!
-      like: liker,
-    })
-    .then(() => {
-      console.log(`${liker} liker saved on database`);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+  const idRef = db.collection(galleryName).doc(imgId);
+  const snapshot = await idRef.get();
+
+  const updatedLike = snapshot.data().like;
+
+  updatedLike.push(liker);
+  idRef.update({ like: updatedLike });
 };
