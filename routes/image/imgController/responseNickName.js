8cfe733a903818ae3c4150af6287e1b5320bf92b
alexjun12 <alexjun12@naver.com>
2022-09-16 19:49:19 +0900

    last error fix

--- routes/image/imgController/responseNickName.js
+++ routes/image/imgController/responseNickName.js
@@ -3,7 +3,7 @@ const admin = require("../../../Configuration/firebaseAuthConfig");
 const db = admin.firestore();
 
 exports.responseNickName = async (req, res) => {
-  const token = req.body.loginUserId;
+  const loginUserId = req.body.loginUserId;
 
   const refer = db.collection("users").doc(loginUserId);
   const snapshot = await refer.get();
